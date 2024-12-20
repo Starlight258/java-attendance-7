965432a2c6e394d49c866a37a9509b402db25faf
Mint <audwl03071@gmail.com>
2024-12-20 22:59:16 +0900

    refactor: 기능 반복마다 공백 라인 추가하여 출력

--- src/main/java/attendance/controller/AttendanceController.java
+++ src/main/java/attendance/controller/AttendanceController.java
@@ -5,7 +5,6 @@ import attendance.dto.CrewDto;
 import attendance.dto.InformDto;
 import attendance.dto.ModifyDto;
 import attendance.dto.MonthTotalAttendanceDto;
-import attendance.exception.ExceptionHandler;
 import attendance.service.AttendanceService;
 import attendance.util.TimeUtils;
 import attendance.view.InputView;
@@ -19,15 +18,12 @@ public class AttendanceController {
 
     private final InputView inputView;
     private final OutputView outputView;
-    private final ExceptionHandler exceptionHandler;
     private final AttendanceService attendanceService;
 
     public AttendanceController(final InputView inputView, final OutputView outputView,
-                                final ExceptionHandler exceptionHandler,
                                 final AttendanceService attendanceService) {
         this.inputView = inputView;
         this.outputView = outputView;
-        this.exceptionHandler = exceptionHandler;
         this.attendanceService = attendanceService;
     }
 
@@ -40,6 +36,7 @@ public class AttendanceController {
             if (command == Command.QUIT) {
                 return;
             }
+            outputView.showBlank();
         }
     }
 
@@ -86,20 +83,6 @@ public class AttendanceController {
         outputView.showInformModify(modifyDto);
     }
 
-    // 오늘은 12월 13일 금요일입니다. 기능을 선택해 주세요.
-    //1. 출석 확인
-    //2. 출석 수정
-    //3. 크루별 출석 기록 확인
-    //4. 제적 위험자 확인
-    //Q. 종료
-    //1
-    //
-    //닉네임을 입력해 주세요.
-    //이든
-    //등교 시간을 입력해 주세요.
-    //09:59
-    //
-    //12월 13일 금요일 09:59 (출석)
     private void checkAttendance(final LocalDateTime now) {
         attendanceService.checkAttendanceDate(now);
         outputView.showRequestCheckNickname();
